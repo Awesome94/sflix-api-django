127224008efc8e7b0ea6b9b60dd3a6e3aa7dc3e7
awesome94 <david.kironde@andela.com>
2019-04-08 23:42:10 +0300

    add representation for users models and fullname

--- sflix/users/models.py
+++ sflix/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+# from Phonenumber_field.modelfields import PhoneNumberField
+from django.utils import timezone
 
 # Create your models here.
+
+class Users(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.TextField()
+    email = models.CharField(max_length=100, blank=True, default='')
+    # phone = PhoneNumberField()
+
+    def full_name(self):
+        return '{} {}'.format(self.frist_name, self.last_name)
+
+    def __str__(self):
+        return self.full_name()
