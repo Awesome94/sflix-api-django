66f9dfe44546c717c458800a9f082f730b2d2300
awesome94 <david.kironde@andela.com>
2019-04-09 23:10:44 +0300

    add jwt authentication as the default authentication class

--- sflix/sflix/settings.py
+++ sflix/sflix/settings.py
@@ -78,6 +78,9 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication'
     ]
 }
 
