2d0fb036e857a85f7b9b85c6c6cd30a85927118e
awesome94 <david.kironde@andela.com>
2019-04-09 22:53:24 +0300

    add users initial view

--- sflix/users/views.py
+++ sflix/users/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Users
-
+from .serializers import UsersSerializer
+from rest_framework import generics
 # Create your views here.
 
-def all_users(request):
-    pass
+class ListUsersView(generics.ListAPIView):
+    """
+    Provides a get method handler.
+    """
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
